0faa3f8d33fc5215d50163f0dac1037a4a0d08c4
mproncace <mproncace@gmail.com>
2014-12-16 15:16:53 -0500

    Finish up mechanism for adding repos

--- src/main/java/net/amigocraft/mpt/Main.java
+++ src/main/java/net/amigocraft/mpt/Main.java
@@ -1,25 +1,27 @@
 /*
  * MPT (Map Packaging Tool)
  *
- * Copyright (c) 2014 Maxim Roncace
+ * Copyright (c) 2014 Maxim Roncacé <https://github.com/mproncace>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * The MIT License (MIT)
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is
+ *     furnished to do so, subject to the following conditions:
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ *     The above copyright notice and this permission notice shall be included in all
+ *     copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *     SOFTWARE.
  */
 package net.amigocraft.mpt;
 
@@ -36,8 +38,8 @@ public class Main extends JavaPlugin {
 	public static Logger log; // logger instance
 
 	public static Gson gson = null;
-	public static JsonObject repos = null; // repo store
-	public static JsonObject packages = null; // package store
+	public static JsonObject repoStore = null; // repo store
+	public static JsonObject packageStore = null; // package store
 
 	@Override
 	public void onEnable(){
@@ -46,18 +48,18 @@ public class Main extends JavaPlugin {
 
 		gson = new GsonBuilder().setPrettyPrinting().create(); // so the stores look decent when saved to disk
 
-		File reposFile = new File(getDataFolder(), "repositories.yml");
-		if (reposFile.exists()){ // repo store has been initialized
+		File rStoreFile = new File(getDataFolder(), "repositories.yml");
+		if (rStoreFile.exists()){ // repo store has been initialized
 			log.info("Loading local repository store...");
 			try {
 				String line;
 				StringBuilder builder = new StringBuilder(); // create a builder
-				while ((line = new BufferedReader(new FileReader(reposFile)).readLine()) != null){ // read line-by-line
+				while ((line = new BufferedReader(new FileReader(rStoreFile)).readLine()) != null){ // read line-by-line
 					builder.append(line); // build
 				}
 				String content = builder.toString();
 				JsonParser parser = new JsonParser(); // create a new parser
-				repos = parser.parse(content).getAsJsonObject(); // parse the read string into a JSON object
+				repoStore = parser.parse(content).getAsJsonObject(); // parse the read string into a JSON object
 			}
 			catch (IOException ex){
 				ex.printStackTrace();
@@ -65,34 +67,21 @@ public class Main extends JavaPlugin {
 			}
 		}
 		else { // we need to initialize the repo store
-			log.info("Initializing local repository store...");
-
-			JsonArray repoArray = new JsonArray(); // create an empty array
-			repos = new JsonObject(); // create an empty object
-			repos.add("repositories", repoArray); // add the array to it
-			try {
-				reposFile.createNewFile(); // create the file
-				BufferedWriter writer = new BufferedWriter(new FileWriter(reposFile)); // get a writer
-				writer.write(repos.toString()); // convert the JSON object to a string and write it
-			}
-			catch (IOException ex){
-				ex.printStackTrace();
-				log.severe("Failed to initialize repository store!");
-			}
+			initializeRepoStore(rStoreFile);
 		}
 
-		File packagesFile = new File(getDataFolder(), "maps.yml");
-		if (packagesFile.exists()){ // package store has been initialized
+		File pStoreFile = new File(getDataFolder(), "maps.yml");
+		if (pStoreFile.exists()){ // package store has been initialized
 			log.info("Loading local package store...");
 			try {
 				String line;
 				StringBuilder builder = new StringBuilder(); // create a builder
-				while ((line = new BufferedReader(new FileReader(packagesFile)).readLine()) != null){ // read
+				while ((line = new BufferedReader(new FileReader(pStoreFile)).readLine()) != null){ // read
 					builder.append(line); // build
 				}
 				String content = builder.toString();
 				JsonParser parser = new JsonParser(); // create a new parser
-				packages = parser.parse(content).getAsJsonObject(); // parse the read string into a JSON object
+				packageStore = parser.parse(content).getAsJsonObject(); // parse the read string into a JSON object
 			}
 			catch (IOException ex){
 				ex.printStackTrace();
@@ -100,19 +89,7 @@ public class Main extends JavaPlugin {
 			}
 		}
 		else { // we need to initialize the package store
-			log.info("Initializing local package store...");
-			JsonArray packageArray = new JsonArray(); // create an empty array
-			packages = new JsonObject(); // create an empty object
-			packages.add("packages", packageArray); // add the array to it
-			try {
-				packagesFile.createNewFile(); // create the file
-				BufferedWriter writer = new BufferedWriter(new FileWriter(packagesFile)); // get a writer
-				writer.write(packages.toString()); // convert the JSON object to a string and write it
-			}
-			catch (IOException ex){
-				ex.printStackTrace();
-				log.severe("Failed to initialize package store!");
-			}
+			initializePackageStore(pStoreFile);
 		}
 
 		this.getCommand("mpt").setExecutor(new CommandManager()); // register the CommandManager class
@@ -122,9 +99,45 @@ public class Main extends JavaPlugin {
 
 	@Override
 	public void onDisable(){
-		log.info(this + " has been disabled!");
+		gson = null;
+		repoStore = null;
+		packageStore = null;
 		log = null;
 		plugin = null;
+		getLogger().info(this + " has been disabled!");
+	}
+
+	public static void initializeRepoStore(File file){
+		log.info("Initializing local repository store...");
+
+		JsonArray repoArray = new JsonArray(); // create an empty array
+		repoStore = new JsonObject(); // create an empty object
+		repoStore.add("repositories", repoArray); // add the array to it
+		try {
+			file.createNewFile(); // create the file
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file)); // get a writer
+			writer.write(repoStore.toString()); // convert the JSON object to a string and write it
+		}
+		catch (IOException ex){
+			ex.printStackTrace();
+			log.severe("Failed to initialize repository store!");
+		}
+	}
+
+	public static void initializePackageStore(File file){
+		log.info("Initializing local package store...");
+		JsonArray packageArray = new JsonArray(); // create an empty array
+		packageStore = new JsonObject(); // create an empty object
+		packageStore.add("packages", packageArray); // add the array to it
+		try {
+			file.createNewFile(); // create the file
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file)); // get a writer
+			writer.write(packageStore.toString()); // convert the JSON object to a string and write it
+		}
+		catch (IOException ex){
+			ex.printStackTrace();
+			log.severe("Failed to initialize package store!");
+		}
 	}
 
 }
--- src/main/java/net/amigocraft/mpt/command/AddRepositoryCommand.java
+++ src/main/java/net/amigocraft/mpt/command/AddRepositoryCommand.java
@@ -1,34 +1,41 @@
 /*
  * MPT (Map Packaging Tool)
  *
- * Copyright (c) 2014 Maxim Roncace
+ * Copyright (c) 2014 Maxim Roncacé <https://github.com/mproncace>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * The MIT License (MIT)
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is
+ *     furnished to do so, subject to the following conditions:
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ *     The above copyright notice and this permission notice shall be included in all
+ *     copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *     SOFTWARE.
  */
 package net.amigocraft.mpt.command;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import net.amigocraft.mpt.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class AddRepositoryCommand extends SubcommandManager {
 
@@ -40,34 +47,99 @@ public class AddRepositoryCommand extends SubcommandManager {
 	public void handle(){
 		if (sender.hasPermission("mpt.addrepo")){
 			if (args.length == 3){
-				String id = args[1];
-				String url = args[2];
+				final String id = args[1];
+				final String path = args[2];
 				// get the main array from the JSON object
-				JsonArray array = Main.repos.getAsJsonArray("repositories");
+				JsonArray array = Main.repoStore.getAsJsonArray("repositories");
 				// verify the repo hasn't already been added
-				for (JsonElement e : array){
+				for (JsonElement e : array){ // iterate repos in local store
 					JsonObject o = e.getAsJsonObject();
 					// check ID
 					if (o.has("id") && o.get("id").getAsString().equalsIgnoreCase(id)){
-						sender.sendMessage(ChatColor.RED + "A repository by that ID has already been added!");
+						sender.sendMessage(ChatColor.RED + "[MPT] A repository by that ID has already been added!");
 						return;
 					}
 					// check URL
-					if (o.has("url") && o.get("url").getAsString().equalsIgnoreCase(url)){
-						sender.sendMessage(ChatColor.RED + "The repository at that URL has already been added!");
+					if (o.has("url") && o.get("url").getAsString().equalsIgnoreCase(path)){
+						sender.sendMessage(ChatColor.RED + "[MPT] The repository at that URL has already been added!");
 						return;
 					}
-					//TODO: verify remote is repository
+					// no way we're making the main thread wait for us to open and read the stream
+					Bukkit.getScheduler().runTaskAsynchronously(Main.plugin, new Runnable(){
+						public void run(){
+							sender.sendMessage(ChatColor.DARK_PURPLE + "[MPT] Attempting connection to repository...");
+							connectAndStore(id, path); // in separate method for organization purposes
+						}
+					});
 				}
 			}
 			else if (args.length < 3)
-				sender.sendMessage(ChatColor.RED + "Too few arguments! Type " + ChatColor.DARK_PURPLE + "/mpt help " +
-						ChatColor.RED + "for help");
+				sender.sendMessage(ChatColor.RED + "[MPT] Too few arguments! Type " + ChatColor.DARK_PURPLE +
+						"/mpt help " + ChatColor.RED + "for help");
 			else
-				sender.sendMessage(ChatColor.RED + "Too many arguments! Type " + ChatColor.DARK_PURPLE + "/mpt help " +
-						ChatColor.RED + "for help");
+				sender.sendMessage(ChatColor.RED + "[MPT] Too many arguments! Type " + ChatColor.DARK_PURPLE +
+						"/mpt help " + ChatColor.RED + "for help");
 		}
 		else
-			sender.sendMessage(ChatColor.RED + "You do not have access to this command!");
+			sender.sendMessage(ChatColor.RED + "[MPT] You do not have access to this command!");
+	}
+
+	private void connectAndStore(String id, String path){
+		try {
+			URL url = new URL(path + (!path.endsWith("/") ? "/" : "") + "mpt.json"); // get URL object for data file
+			URLConnection conn = url.openConnection();
+			if (conn instanceof HttpURLConnection){
+				HttpURLConnection http = (HttpURLConnection)conn; // cast the connection
+				int response = http.getResponseCode(); // get the response
+				if (response >= 200 && response <= 299){ // verify the remote isn't upset at us
+					InputStream is = http.getInputStream(); // open a stream to the URL
+					BufferedReader reader = new BufferedReader(new InputStreamReader(is)); // get a reader
+					JsonParser parser = new JsonParser(); // get a new parser
+					JsonObject json = parser.parse(reader).getAsJsonObject(); // parse JSON object from stream
+					// vefify remote config is valid
+					if (json.has("name") && json.has("packages") && json.get("packages").isJsonArray()){
+						String name = json.get("name").getAsString(); // get official name from remote
+						try { // inner try block is necessary so local issues aren't misreported as remote
+							File store = new File(Main.plugin.getDataFolder(), "repositories.json");
+							if (!store.exists())
+								Main.initializeRepoStore(store); // gotta initialize it before using it
+							JsonObject repoElement = new JsonObject(); // create a new JSON object
+							repoElement.addProperty("id", id); // set the repo ID (user-defined)
+							repoElement.addProperty("name", name); // set the repo name (determined by remote config)
+							repoElement.addProperty("url", path); // set the repo URL
+							Main.repoStore.getAsJsonArray("repositories").add(repoElement); // add to local store object
+							FileWriter writer = new FileWriter(store); // get a writer for the store file
+							writer.write(Main.gson.toJson(Main.repoStore)); // write to disk
+							// apt-get doesn't fetch packages when a repo is added, so I'm following that precedent
+							sender.sendMessage(ChatColor.DARK_PURPLE + "[MPT] Successfully added repository to local " +
+									"store!  You may now use " + ChatColor.GOLD + "/mpt update" +
+									ChatColor.DARK_PURPLE + " to fetch available packages.");
+						}
+						catch (IOException ex){
+							sender.sendMessage(ChatColor.RED + "[MPT] Failed to add repository to local store!");
+						}
+					}
+					else
+						sender.sendMessage(ChatColor.RED + "[MPT] Repository index is missing required elements!");
+				}
+				else {
+					sender.sendMessage(ChatColor.RED + "[MPT] Remote returned bad response code! (" + response + ")");
+					if (!http.getResponseMessage().isEmpty())
+						sender.sendMessage(ChatColor.RED + "[MPT] The remote says: " + ChatColor.GRAY +
+								ChatColor.ITALIC + http.getResponseMessage());
+				}
+			}
+			else
+				sender.sendMessage(ChatColor.RED + "[MPT] Bad protocol for URL!");
+		}
+		catch (MalformedURLException ex){
+			sender.sendMessage(ChatColor.RED + "[MPT] Cannot parse URL!");
+		}
+		catch (IOException ex){
+			sender.sendMessage(ChatColor.RED + "[MPT] Cannot open connection to URL!");
+		}
+		catch (JsonParseException ex){
+			sender.sendMessage(ChatColor.RED + "[MPT] Repository index is not valid JSON!");
+		}
 	}
 }

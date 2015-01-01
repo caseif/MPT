060b4d022838df73335a339216b96a0816a93028
mproncace <mproncace@gmail.com>
2015-01-01 16:40:42 -0500

    Add getPackageInfo(String) method to API and improve documentation

--- src/main/java/net/amigocraft/mpt/MptApi.java
+++ src/main/java/net/amigocraft/mpt/MptApi.java
@@ -38,6 +38,7 @@ public class MptApi {
 	 * @param id the ID of the package to install
 	 * @throws MPTException if something goes wrong while downloading or installing the package.
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static void installPackage(String id) throws MPTException {
 		InstallCommand.installPackage(id);
@@ -45,9 +46,11 @@ public class MptApi {
 
 	/**
 	 * Remove a package by the given ID.
+	 * <strong>This method may not be called from the main thread.</strong>
 	 * @param id the ID of the package to install
 	 * @throws MPTException if something goes wrong while removing the package.
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static void removePackage(String id) throws MPTException {
 		RemoveCommand.removePackage(id);
@@ -60,6 +63,7 @@ public class MptApi {
 	 * @return the new version of the package, or null if it is already up-to-date
 	 * @throws MPTException if something goes wrong while removing or reinstalling the package.
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static String upgradePackage(String id) throws MPTException {
 		return UpgradeCommand.upgradePackage(id);
@@ -72,6 +76,7 @@ public class MptApi {
 	 * currently installed version.
 	 * @throws MPTException if something goes wrong while indexing the installed packages
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static List<String[]> getInstalledPackages() throws MPTException {
 		return ListInstalledCommand.getPackages();
@@ -84,9 +89,27 @@ public class MptApi {
 	 * currently installed version.
 	 * @throws MPTException if something goes wrong while indexing the installed packages
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static List<String[]> getAvailablePackages() throws MPTException {
 		return ListAvailableCommand.getPackages();
+	}
+
+	/**
+	 * Retrieves detailed information about a specific package.
+	 * @param id the package to retrieve information for
+	 * @return an array containing information about the package.
+	 * The first array element contains its "friendly" name,
+	 * the second its description,
+	 * the third its latest version,
+	 * the fourth its content URL,
+	 * the fifth its SHA-1 checksum (null if not specified),
+	 * and the sixth its currently installed version (null if not installed).
+	 * @throws MPTException if something goes wrong while reading the package's information
+	 * @since 1.2.0
+	 */
+	public static String[] getPackageInfo(String id) throws MPTException {
+		return InfoCommand.getPackageInfo(id);
 	}
 
 	/**
@@ -96,6 +119,7 @@ public class MptApi {
 	 * @return the ID of the newly-added repository
 	 * @throws MPTException if something goes wrong while connecting to or parsing the repository.
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static String addRepository(String url) throws MPTException {
 		return AddRepositoryCommand.addRepository(url);
@@ -106,6 +130,7 @@ public class MptApi {
 	 * @param id the ID of the repository to remove
 	 * @throws MPTException if something goes wrong while removing the repository.
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static void removeRepository(String id) throws MPTException {
 		RemoveRepositoryCommand.removeRepository(id);
@@ -116,6 +141,7 @@ public class MptApi {
 	 * <strong>This method may not be called from the main thread.</strong>
 	 * @throws MPTException if something goes wrong while connecting to or parsing the repository.
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static void updatePackageStore() throws MPTException {
 		UpdateCommand.updateStore();
@@ -127,6 +153,7 @@ public class MptApi {
 	 * The first index is the repository's ID, and the second is its URL.
 	 * @throws MPTException if something goes wrong while connecting to or parsing the repository.
 	 * A specific error message will be included in the exception.
+	 * @since 1.0.0
 	 */
 	public static List<String[]> getRepositories() throws MPTException {
 		return ListRepositoriesCommand.getRepositories();
